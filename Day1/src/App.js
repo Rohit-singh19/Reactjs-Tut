0f1436a163945fd8f2d1e569fa5e81908f8fbccb
Rohit singh <77694147+Rohit-singh19@users.noreply.github.com>
2021-07-17 23:05:53 +0530

    Update App.js

--- Day1/src/App.js
+++ Day1/src/App.js
@@ -1,11 +1,8 @@
-import React,{useState} from "react"
+import React from "react"
 import "./style.css"
 import Button from "./Button"
-// import Counterbutton from "./Counterbutton"
 
 function Apple() {
-
-    let[score, setScore] = useState(0)
     return(
         <div>
             <section className="header">
@@ -17,25 +14,8 @@ function Apple() {
                 <Button buttonText="Counter"/>
                 <Button buttonText="Lets Upgrade"/>
             </section>
-
-            <section className="counter">
-                <h2>Counter App</h2>
-
-                <h3> The value of score is <span>{score}</span></h3>
-
-                {/* <Counterbutton counterbuttonText="Increment"  onClick={()=>{setScore(score+1)}}/>
-                <Counterbutton counterbuttonText="Decrement"/>
-                <Counterbutton counterbuttonText="Reset"/>  */}
-
-                <div className="btns">
-                    <button onClick ={()=>(score<25)? setScore(score+1) : ""}>Increment</button>
-                    <button onClick={()=>(score>0)? setScore(score-1) : "" }>Decrement</button>
-                    <button onClick= {()=>{setScore(0)}}>Reset</button>
-                </div>
-            </section>
         </div>
     )
 }
 
 export default Apple
-
