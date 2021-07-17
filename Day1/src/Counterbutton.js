0e5bf965c6e80191619578f4117cad36970c7abd
Rohit singh <77694147+Rohit-singh19@users.noreply.github.com>
2021-07-17 23:05:14 +0530

    Add files via upload

---
+++ Day1/src/Counterbutton.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Counterbutton = ({counterbuttonText})=>{
+    return(
+    <button className="counter-button"> {counterbuttonText} </button>
+    )
+}
+
+export default Counterbutton
